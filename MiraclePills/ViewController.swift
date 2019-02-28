802a8214fb2c9ec9fcbea040473ae334e296d69b
Eugenio Contreras Sánchez <ecs.keni@gmail.com>
2019-02-28 14:46:23 -0300

    First commit

---
+++ MiraclePills/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiraclePills
+//
+//  Created by Eugenio Contreras on 2/28/19.
+//  Copyright © 2019 Eugenio Contreras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
